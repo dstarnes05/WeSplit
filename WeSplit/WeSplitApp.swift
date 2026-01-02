18ab00879abf4bb4c95a2bd7781f24188b713ec8
Daniel Starnes <dstarnes261@gmail.com>
2026-01-02 10:06:19 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Daniel Starnes on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
